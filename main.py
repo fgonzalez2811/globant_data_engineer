4e17aa8c0d2b8300fbcb838da89be67f0c79d1ae
Felipe <fgonzalez2811@gmail.com>
2025-02-22 18:45:37 -0500

    created Dockerfile and docker-compose to containerize fastAPI app and PostgreSQL database, updated requirements.txt with dependencies needed for postreSQL

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 @app.get('/')
 def index():
-    return {'data':'Hello world'}
+    return {'data':'Hello Globant'}
