924f8c9065b61aafbb01e9d40f3597e1f0647c03
Felipe <fgonzalez2811@gmail.com>
2025-02-23 17:01:36 -0500

    Updated Dockerfile and requirements.txt to include dependencies needed to install psycopg2 for postgreSQL database interaction. Updated main.py to test connection with PostgreSQL DB

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
 FROM python:3.10-slim
 
+# Install system dependencies for psycopg2
+RUN apt-get update && apt-get install -y \
+    libpq-dev \
+    gcc \
+    python3-dev
+
 WORKDIR /app
 
 COPY requirements.txt .
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+import psycopg2
 
 app = FastAPI()
 
@@ -7,4 +8,13 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 @app.get('/')
 def index():
-    return {'data':'Hello Globant'}
+    conn = psycopg2.connect(DATABASE_URL)
+    print(conn)
+    cur = conn.cursor()
+    cur.execute("SELECT 'Postgre Responding'")
+    message = cur.fetchall()
+    cur.close()
+    conn.close()
+    return {'data':'Hello Globant', 'message':message}
+
+    
